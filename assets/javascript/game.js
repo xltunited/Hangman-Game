af56306f10e0a0b63c3d572794548871fe309c83
Eddy Leon <xltunited@gmail.com>
2016-08-23 17:50:40 -0400

    fixd heroku api call

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -137,7 +137,7 @@ $(document).ready(function(){
 
 		//var that holds the url to the API call, this one holds my own requested API key
 
-		var apicall = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=ba3d180eb7c56867e600204177a00eee809035f45d0789fd6";
+		var apicall = "https://crossorigin.me/http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=ba3d180eb7c56867e600204177a00eee809035f45d0789fd6";
 
 		//Use GET request to get a 5 character word
 
